fd764a068d6cc2c5dfa0479e9ea89374b049a6b0
Getaye Temesgen <tgech71@gmail.com>
2026-01-01 08:43:12 +0000

    { key: "X-Content-Type-Options", value: "nosniff" },  please add this to

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
         ],
       },
     ]
